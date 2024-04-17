177ba6409a9878726d808423671c1ad3da9fb3d7
mia love <122865270+mia336love@users.noreply.github.com>
2024-04-18 01:03:47 +0300

    demical added
    Frontal Impact

--- 4/fourth.js
+++ 4/fourth.js
@@ -4,6 +4,7 @@ let num1 = ''
 let num2 = ''
 let operator = ''
 
+let decimalAdded = false
 
 calculator.addEventListener('click', (event) => {
     if (event.target.classList.contains('num')) {
@@ -12,8 +13,18 @@ calculator.addEventListener('click', (event) => {
         } else if (operator !== '') {
             num2 += event.target.textContent
         }
+    } else if (event.target.classList.contains('dot')) {
+        if (!decimalAdded && (num1 !== '' || num2 !== '')) {
+            decimalAdded = true;
+            if (operator === '') {
+                num1 += event.target.textContent;
+            } else if (operator !== '') {
+                num2 += event.target.textContent;
+            }
+        }
     } else if (event.target.classList.contains('oper')) {
         operator = event.target.textContent
+        decimalAdded = false;
     } else if (event.target.classList.contains('equals')) {
         const result = calculate(num1, operator, num2)
 
@@ -24,26 +35,32 @@ calculator.addEventListener('click', (event) => {
 })
 
 let calculate = (num1, operator, num2) => {
-    switch (operator) {
-        case '+':
-            return Number(num1) + Number(num2);
+    try {
+        switch (operator) {
+            case '+':
+                return Number(num1) + Number(num2);
 
-        case '-':
-            return Number(num1) - Number(num2);
+            case '-':
+                return Number(num1) - Number(num2);
 
-        case '×':
-            return Number(num1) * Number(num2);
+            case '×':
+                return Number(num1) * Number(num2);
 
-        case '÷':
-            return Number(num1) / Number(num2);
+            case '÷':
+                return Number(num1) / Number(num2);
 
-        default:
-            break;
+            default:
+                break;
+        }
+    } catch (error) {
+
     }
+
 }
 
 let clearInput = () => {
     num1 = ''
     num2 = ''
     operator = ''
+    decimalAdded = false
 }

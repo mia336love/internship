fbacfaaf47d310fa3641f4c7ac25c872742e9fd6
mia love <122865270+mia336love@users.noreply.github.com>
2024-08-28 04:57:09 +0300

    cats become a class

--- React/8/internship-app/src/Components/CatsPics/CatsPics.jsx
+++ React/8/internship-app/src/Components/CatsPics/CatsPics.jsx
@@ -1,16 +1,46 @@
-import './CatsPics.css'
+import "./CatsPics.css";
 
-function CatsPics() {
-  return <div className="cats-pics-main">
-    
-    <img id="cat1" alt='cat1' src="https://i.pinimg.com/564x/69/17/7f/69177f2d928d3be2c3b230634335a3b4.jpg"></img>
-    <img id="cat2" alt='cat2' src="https://i.pinimg.com/736x/57/49/ae/5749aec49970cb9bd9ab456a920610cc.jpg"></img>
-    <img id="cat3" alt='cat3' src="https://i.pinimg.com/736x/71/07/c8/7107c82a39b5b2c14f6c9d44625be372.jpg"></img>
-    <img id="cat4" alt='cat4' src="https://i.pinimg.com/564x/9e/3d/70/9e3d702963c45425f74979d85cfe38aa.jpg"></img>
-    <img id="cat5" alt='cat5' src="https://i.pinimg.com/736x/5a/55/98/5a5598ab6cb1aaa3ae7d3d7f40861b99.jpg"></img>
-    <img id="cat6" alt='cat6' src="https://i.pinimg.com/736x/a3/94/e7/a394e7b5f57eb7791d4a57a92370c7b1.jpg"></img>
+import React, { Component } from "react";
 
-  </div>;
+class CatsPics extends Component {
+  render() {
+    return (
+      <div className="cats-pics-main">
+        <img
+          id="cat1"
+          alt="cat1"
+          src="https://i.pinimg.com/564x/69/17/7f/69177f2d928d3be2c3b230634335a3b4.jpg"
+        ></img>
+        <img
+          id="cat2"
+          alt="cat2"
+          src="https://i.pinimg.com/736x/57/49/ae/5749aec49970cb9bd9ab456a920610cc.jpg"
+        ></img>
+        <img
+          id="cat3"
+          alt="cat3"
+          src="https://i.pinimg.com/736x/71/07/c8/7107c82a39b5b2c14f6c9d44625be372.jpg"
+        ></img>
+        <img
+          id="cat4"
+          alt="cat4"
+          src="https://i.pinimg.com/564x/9e/3d/70/9e3d702963c45425f74979d85cfe38aa.jpg"
+        ></img>
+        <img
+          id="cat5"
+          alt="cat5"
+          src="https://i.pinimg.com/736x/5a/55/98/5a5598ab6cb1aaa3ae7d3d7f40861b99.jpg"
+        ></img>
+        <img
+          id="cat6"
+          alt="cat6"
+          src="https://i.pinimg.com/736x/a3/94/e7/a394e7b5f57eb7791d4a57a92370c7b1.jpg"
+        ></img>
+        //{" "}
+      </div>
+    );
+  }
 }
+
 
 export default CatsPics;
